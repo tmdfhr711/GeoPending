c5fdc630ee4a480dcbd4bd23686a3a2cff98a276
David.OH <dev_david@naver.com>
2018-04-15 02:33:30 +0900

    #001. 로그인 기능 구현
    1. FirebaseAuth 연동

--- app/src/main/java/com/plplim/david/geopending/LoginActivity.java
+++ app/src/main/java/com/plplim/david/geopending/LoginActivity.java
@@ -1,13 +1,93 @@
 package com.plplim.david.geopending;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private EditText loginEmailText;
+    private EditText loginPassText;
+    private Button loginButton;
+    private Button loginRegButton;
+
+    private FirebaseAuth mAuth;
+
+    private ProgressBar loginProgress;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        loginEmailText = (EditText) findViewById(R.id.login_text_email);
+        loginPassText = (EditText) findViewById(R.id.login_text_password);
+        loginButton = (Button) findViewById(R.id.login_btn_login);
+        loginRegButton = (Button) findViewById(R.id.login_btn_reg);
+        loginProgress = (ProgressBar) findViewById(R.id.login_progress);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String loginEmail = loginEmailText.getText().toString();
+                String loginPass = loginPassText.getText().toString();
+
+                if (!TextUtils.isEmpty(loginEmail) && !TextUtils.isEmpty(loginPass)) {
+                    loginProgress.setVisibility(View.VISIBLE);
+
+                    mAuth.signInWithEmailAndPassword(loginEmail, loginPass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+
+                            if (task.isSuccessful()) {
+
+                                sendToMain();
+
+                            } else {
+
+                                String errorMessage = task.getException().getMessage();
+                                Toast.makeText(LoginActivity.this, ":Error : " + errorMessage, Toast.LENGTH_SHORT).show();
+
+                            }
+
+                            loginProgress.setVisibility(View.INVISIBLE);
+
+                        }
+                    });
+                }
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+
+        if (currentUser != null) {
+            sendToMain();
+        }
+    }
+
+    private void sendToMain() {
+        Intent mainIntent = new Intent(LoginActivity.this, MainActivity.class);
+        startActivity(mainIntent);
+        finish();
     }
 }
---
+++ app/src/main/java/com/plplim/david/geopending/MainActivity.java
@@ -0,0 +1,32 @@
+package com.plplim.david.geopending;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        FirebaseAuth.getInstance().signOut();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
+        if (currentUser == null) {
+            Intent loginIntent = new Intent(MainActivity.this, LoginActivity.class);
+            startActivity(loginIntent);
+            finish();
+        } else {
+
+        }
+    }
+}
