37c50f2958bd127d949c933cf71e54a09f434ef3
David.OH <dev_david@naver.com>
2018-04-14 20:11:50 +0900

    #000. Splash 및 Login 화면구성
    1. FirebaseRemoteConfig 연동

---
+++ app/src/main/java/com/plplim/david/geopending/SplashActivity.java
@@ -0,0 +1,75 @@
+package com.plplim.david.geopending;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Color;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.WindowManager;
+import android.widget.LinearLayout;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.remoteconfig.FirebaseRemoteConfig;
+import com.google.firebase.remoteconfig.FirebaseRemoteConfigSettings;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private LinearLayout linearLayout;
+    private FirebaseRemoteConfig firebaseRemoteConfig;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        linearLayout = (LinearLayout) findViewById(R.id.splash_linearlayout);
+
+        firebaseRemoteConfig = FirebaseRemoteConfig.getInstance();
+        FirebaseRemoteConfigSettings configSettings = new FirebaseRemoteConfigSettings.Builder()
+                .setDeveloperModeEnabled(BuildConfig.DEBUG)
+                .build();
+        firebaseRemoteConfig.setConfigSettings(configSettings);
+        firebaseRemoteConfig.setDefaults(R.xml.default_config);
+        firebaseRemoteConfig.fetch(0)
+                .addOnCompleteListener(this, new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()) {
+                            firebaseRemoteConfig.activateFetched();
+                        } else {
+
+                        }
+                        displayMessage();
+                    }
+                });
+    }
+    private void displayMessage() {
+        String splash_background = firebaseRemoteConfig.getString("splash_background");
+        boolean caps = firebaseRemoteConfig.getBoolean("splash_message_caps");
+        String splash_message = firebaseRemoteConfig.getString("splash_message");
+
+        linearLayout.setBackgroundColor(Color.parseColor(splash_background));
+
+        if (caps) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setMessage(splash_message).setPositiveButton("확인", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    finish();
+
+                }
+            });
+
+            builder.create().show();
+        } else {
+            startActivity(new Intent(this, LoginActivity.class));
+            finish();
+        }
+
+
+    }
+}
