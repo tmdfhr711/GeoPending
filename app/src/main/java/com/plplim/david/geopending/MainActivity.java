00c9f38ac9e00f61ce743d85f4dca596326a7369
David.OH <dev_david@naver.com>
2018-04-26 18:23:05 +0900

    #008. 구글맵연동

--- app/src/main/java/com/plplim/david/geopending/MainActivity.java
+++ app/src/main/java/com/plplim/david/geopending/MainActivity.java
@@ -2,37 +2,17 @@ package com.plplim.david.geopending;
 
 import android.content.Intent;
 import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
-import android.support.design.internal.BottomNavigationItemView;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
-import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.firestore.DocumentChange;
-import com.google.firebase.firestore.DocumentSnapshot;
-import com.google.firebase.firestore.EventListener;
-import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.FirebaseFirestoreException;
-import com.google.firebase.firestore.QuerySnapshot;
-import com.google.firebase.storage.FirebaseStorage;
-
-import org.w3c.dom.Document;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -40,12 +20,6 @@ public class MainActivity extends AppCompatActivity {
 
     private Toolbar mainToolbar;
     private FirebaseAuth mAuth;
-
-    private RecyclerView recyclerView;
-    private FirebaseFirestore firebaseFirestore;
-
-    private List<Users> usersList;
-    private UsersListAdapter usersListAdapter;
 
     private BottomNavigationView bottomNavigationView;
 
@@ -65,17 +39,6 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(mainToolbar);
 
         getSupportActionBar().setTitle("Geo Pending");
-
-        usersList = new ArrayList<>();
-        usersListAdapter = new UsersListAdapter(MainActivity.this, usersList);
-
-
-        //recyclerView = (RecyclerView) findViewById(R.id.recyclerview);
-        firebaseFirestore = FirebaseFirestore.getInstance();
-
-        /*recyclerView.setHasFixedSize(true);
-        recyclerView.setAdapter(usersListAdapter);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));*/
 
         peopleFragment = new PeopleFragment();
         mapFragment = new MapFragment();
@@ -100,44 +63,12 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
-
-
-
-
-    }
-
-    public void deleteItem(String deleteKey) {
-        for(int i = 0; i < usersList.size(); i++) {
-            //Log.d(TAG, "DELETE_ITEM\n" + "DeleteKey : " + deleteKey + "\nuserListID : " + item.userId);
-            if (deleteKey.equals(usersList.get(i).userId)) {
-                usersList.remove(i);
-            }
-        }
-    }
-
-    public void modifyItem(String modifyItem, Users users) {
-        /*int index = 0;
-        for (Users item : usersList) {
-            //Log.d(TAG, "MODIFY_ITEM\n" + "DeleteKey : " + modifyItem + "\nuserListID : " + item.userId);
-            if (modifyItem.equals(item.userId)) {
-                usersList.set(index, users);
-            }
-            index++;
-        }*/
-
-        Log.e("MODIFYITEM", users.getName().toString());
-        for(int i = 0; i < usersList.size(); i++) {
-            //Log.d(TAG, "DELETE_ITEM\n" + "DeleteKey : " + deleteKey + "\nuserListID : " + item.userId);
-            if (modifyItem.equals(usersList.get(i).userId)) {
-                usersList.set(i, users);
-            }
-        }
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        firebaseFirestore = null;
+        //firebaseFirestore = null;
     }
 
     @Override
--- app/src/main/java/com/plplim/david/geopending/MapFragment.java
+++ app/src/main/java/com/plplim/david/geopending/MapFragment.java
@@ -2,17 +2,26 @@ package com.plplim.david.geopending;
 
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
 /**
  * A simple {@link Fragment} subclass.
  */
-public class MapFragment extends Fragment {
+public class MapFragment extends Fragment implements OnMapReadyCallback{
 
+    private MapView mapView;
 
     public MapFragment() {
         // Required empty public constructor
@@ -23,7 +32,68 @@ public class MapFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-        return inflater.inflate(R.layout.fragment_map, container, false);
+        View view = inflater.inflate(R.layout.fragment_map, container, false);
+
+        mapView = (MapView) view.findViewById(R.id.mapView);
+        mapView.getMapAsync(this);
+
+        return view;
+    }
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        //액티비티가 처음 생성될 때 실행되는 함수
+
+        if(mapView != null)
+        {
+            mapView.onCreate(savedInstanceState);
+        }
     }
 
+    @Override
+    public void onStart() {
+        super.onStart();
+        mapView.onStart();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        mapView.onStop();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mapView.onPause();
+    }
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapView.onLowMemory();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
+    }
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        LatLng SEOUL = new LatLng(37.56, 126.97);
+        MarkerOptions markerOptions = new MarkerOptions();
+        markerOptions.position(SEOUL);
+        markerOptions.title("서울");
+        markerOptions.snippet("수도");
+        googleMap.addMarker(markerOptions);
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(SEOUL));
+        googleMap.animateCamera(CameraUpdateFactory.zoomTo(13));
+    }
 }
