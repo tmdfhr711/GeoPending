90c39ea253988c487f5eef3450e795b2a8b40821
David.OH <dev_david@naver.com>
2018-04-26 17:29:50 +0900

    #007. Add Fragment
    1. PeopleFragment
    2. MapFragment
    3. AccountFragment

--- app/src/main/java/com/plplim/david/geopending/MainActivity.java
+++ app/src/main/java/com/plplim/david/geopending/MainActivity.java
@@ -5,6 +5,8 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.internal.BottomNavigationItemView;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
@@ -47,6 +49,10 @@ public class MainActivity extends AppCompatActivity {
 
     private BottomNavigationView bottomNavigationView;
 
+    private PeopleFragment peopleFragment;
+    private MapFragment mapFragment;
+    private AccountFragment accountFragment;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -64,12 +70,16 @@ public class MainActivity extends AppCompatActivity {
         usersListAdapter = new UsersListAdapter(MainActivity.this, usersList);
 
 
-        recyclerView = (RecyclerView) findViewById(R.id.recyclerview);
+        //recyclerView = (RecyclerView) findViewById(R.id.recyclerview);
         firebaseFirestore = FirebaseFirestore.getInstance();
 
-        recyclerView.setHasFixedSize(true);
+        /*recyclerView.setHasFixedSize(true);
         recyclerView.setAdapter(usersListAdapter);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));*/
+
+        peopleFragment = new PeopleFragment();
+        mapFragment = new MapFragment();
+        accountFragment = new AccountFragment();
 
         bottomNavigationView = (BottomNavigationView) findViewById(R.id.mainBottomnavigation);
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -77,48 +87,21 @@ public class MainActivity extends AppCompatActivity {
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 switch (item.getItemId()) {
                     case R.id.action_people:
-                        Toast.makeText(MainActivity.this, "people click", Toast.LENGTH_SHORT).show();
+                        replaceFragment(peopleFragment);
                         return true;
                     case R.id.action_map:
-                        Toast.makeText(MainActivity.this, "map click", Toast.LENGTH_SHORT).show();
+                        replaceFragment(mapFragment);
                         return true;
                     case R.id.action_account:
-                        Toast.makeText(MainActivity.this, "account click", Toast.LENGTH_SHORT).show();
+                        replaceFragment(accountFragment);
                         return true;
+                    default :
+                        return false;
                 }
-                return false;
             }
         });
 
-        FirebaseFirestore.getInstance().collection("Users")
-                .addSnapshotListener(new EventListener<QuerySnapshot>() {
-                    @Override
-                    public void onEvent(@Nullable QuerySnapshot queryDocumentSnapshots, @Nullable FirebaseFirestoreException e) {
-                        Log.e(TAG, "FirebaseStore CALLBACK");
-                        if (e != null) {
-                            Log.d(TAG, "Error : " + e.getMessage());
-                        }
 
-                        //usersList.clear();
-                        for (DocumentChange doc : queryDocumentSnapshots.getDocumentChanges()) {
-                            if (doc.getType() == DocumentChange.Type.ADDED) {
-                                String user_id = doc.getDocument().getId();
-                                Users users = doc.getDocument().toObject(Users.class).withId(user_id);
-                                usersList.add(users);
-
-
-                            } else if (doc.getType() == DocumentChange.Type.MODIFIED) {
-                                String userid = doc.getDocument().getId();
-                                Users get_user = doc.getDocument().toObject(Users.class).withId(userid);
-                                modifyItem(userid, get_user);
-                            } else if (doc.getType() == DocumentChange.Type.REMOVED) {
-                                deleteItem(doc.getDocument().getId());
-                            }
-
-                        }
-                        usersListAdapter.notifyDataSetChanged();
-                    }
-                });
 
 
     }
@@ -202,5 +185,11 @@ public class MainActivity extends AppCompatActivity {
     private void logOut() {
         mAuth.signOut();
         sendToLogin();
+    }
+
+    private void replaceFragment(Fragment fragment) {
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.main_container, fragment);
+        fragmentTransaction.commit();
     }
 }
---
+++ app/src/main/java/com/plplim/david/geopending/PeopleFragment.java
@@ -0,0 +1,117 @@
+package com.plplim.david.geopending;
+
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.google.firebase.firestore.DocumentChange;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class PeopleFragment extends Fragment {
+    private final String TAG = "PEOPLE FRAGMENT";
+
+    private RecyclerView recyclerView;
+    private UsersListAdapter usersListAdapter;
+    private FirebaseFirestore firebaseFirestore;
+
+    private List<Users> usersList;
+
+    public PeopleFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_people, container, false);
+
+        recyclerView = view.findViewById(R.id.peopleRecyclerView);
+        firebaseFirestore = FirebaseFirestore.getInstance();
+
+        usersList = new ArrayList<>();
+        usersListAdapter = new UsersListAdapter(container.getContext(), usersList);
+
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setAdapter(usersListAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(container.getContext()));
+
+        FirebaseFirestore.getInstance().collection("Users")
+                .addSnapshotListener(new EventListener<QuerySnapshot>() {
+                    @Override
+                    public void onEvent(@Nullable QuerySnapshot queryDocumentSnapshots, @Nullable FirebaseFirestoreException e) {
+                        Log.e(TAG, "FirebaseStore CALLBACK");
+                        if (e != null) {
+                            Log.d(TAG, "Error : " + e.getMessage());
+                        }
+
+                        //usersList.clear();
+                        for (DocumentChange doc : queryDocumentSnapshots.getDocumentChanges()) {
+                            if (doc.getType() == DocumentChange.Type.ADDED) {
+                                String user_id = doc.getDocument().getId();
+                                Users users = doc.getDocument().toObject(Users.class).withId(user_id);
+                                usersList.add(users);
+
+
+                            } else if (doc.getType() == DocumentChange.Type.MODIFIED) {
+                                String userid = doc.getDocument().getId();
+                                Users get_user = doc.getDocument().toObject(Users.class).withId(userid);
+                                modifyItem(userid, get_user);
+                            } else if (doc.getType() == DocumentChange.Type.REMOVED) {
+                                deleteItem(doc.getDocument().getId());
+                            }
+
+                        }
+                        usersListAdapter.notifyDataSetChanged();
+                    }
+                });
+
+        return view;
+    }
+
+    public void deleteItem(String deleteKey) {
+        for(int i = 0; i < usersList.size(); i++) {
+            //Log.d(TAG, "DELETE_ITEM\n" + "DeleteKey : " + deleteKey + "\nuserListID : " + item.userId);
+            if (deleteKey.equals(usersList.get(i).userId)) {
+                usersList.remove(i);
+            }
+        }
+    }
+
+    public void modifyItem(String modifyItem, Users users) {
+        /*int index = 0;
+        for (Users item : usersList) {
+            //Log.d(TAG, "MODIFY_ITEM\n" + "DeleteKey : " + modifyItem + "\nuserListID : " + item.userId);
+            if (modifyItem.equals(item.userId)) {
+                usersList.set(index, users);
+            }
+            index++;
+        }*/
+
+        Log.e("MODIFYITEM", users.getName().toString());
+        for(int i = 0; i < usersList.size(); i++) {
+            //Log.d(TAG, "DELETE_ITEM\n" + "DeleteKey : " + deleteKey + "\nuserListID : " + item.userId);
+            if (modifyItem.equals(usersList.get(i).userId)) {
+                usersList.set(i, users);
+            }
+        }
+    }
+
+}
